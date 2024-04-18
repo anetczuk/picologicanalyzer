cad7126913a7b94b5837a65dac2ca5444c2cdf5b
anetczuk <anetczuk@o2.pl>
2024-04-18 23:07:43 +0200

    Add tools

---
+++ process-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+$SCRIPT_DIR/doc/generate-doc.sh
+
+$SCRIPT_DIR/tools/mdpreproc.py $SCRIPT_DIR/README.md
+
+$SCRIPT_DIR/tools/checkall.sh
+
+
+echo "generation completed"
